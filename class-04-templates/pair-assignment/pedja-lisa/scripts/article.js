393389b810b112670655fc29dd056d202823f9dc
Predrag Josifovic <pjosifovic@yahoo.com>
2016-02-04 13:23:35 -0800

    adding tripple brackets to 'escape' html tags for body

--- class-04-templates/pair-assignment/pedja-lisa/scripts/article.js
+++ class-04-templates/pair-assignment/pedja-lisa/scripts/article.js
@@ -13,7 +13,8 @@ Article.prototype.toHtml = function() {
   // DONE: Use handlebars to render your articles.
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
-  var template = Handlebars.compile($('#article-template')).text();
+  var template = Handlebars.compile($('#article-template').text());
+
 
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
