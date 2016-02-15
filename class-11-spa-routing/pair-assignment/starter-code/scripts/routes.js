6ccfef87453f719ce7a8e1952d9f82ffef359c67
Predrag Josifovic <pjosifovic@yahoo.com>
2016-02-15 11:41:28 -0800

    adding page.js and setting routes

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,9 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+// DONE: What function do you call to activate page.js? Fire it off now, to execute
+
+page('/', articlesController.index); // ('/')--> takes us to the home page!
+page('/about', aboutController.index);
+
+page();
