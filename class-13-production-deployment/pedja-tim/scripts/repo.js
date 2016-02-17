3f8d4343c68c0370c284c14c124ce26191f02f79
Predrag Josifovic <pjosifovic@yahoo.com>
2016-02-17 13:52:01 -0800

    refactoring request call to GH proxy end point

---
+++ class-13-production-deployment/pedja-tim/scripts/repo.js
@@ -0,0 +1,23 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  // DONE: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  repos.requestRepos = function(callback) {
+    $.get('/github/users/pjosifovic/repos' +
+            '?per_page=5' +
+            '&sort=updated', function(data, message, xhr) {
+        repos.all = data;
+      }
+    }).done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
